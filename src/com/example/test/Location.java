18cbf93fb78b3228980c43c116a8647848edcd19
karimshakir <karimshakir99@gmail.com>
2017-12-31 13:37:21 -0600

    adds class location

--- src/com/example/test/Location.java
+++ src/com/example/test/Location.java
@@ -1,4 +1,29 @@
 package com.example.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+
+    }
+    public void
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return exits;
+    }
 }
--- src/com/example/test/Main.java
+++ src/com/example/test/Main.java
@@ -3,6 +3,6 @@ package com.example.test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	pu
     }
 }
